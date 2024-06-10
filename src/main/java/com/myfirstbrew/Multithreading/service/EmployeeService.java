a9cbb997c099cbb9c855ab5c4a78cf15374ed7f4
Sabyasachi09 <sab.choubey@hotmail.com>
2024-06-10 22:28:16 +0530

    add employee service

---
+++ src/main/java/com/myfirstbrew/Multithreading/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.myfirstbrew.Multithreading.service;
+
+import com.myfirstbrew.Multithreading.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAll();
+
+}
