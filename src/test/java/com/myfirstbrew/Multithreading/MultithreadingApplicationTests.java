89bed9c163ca60888140ac7f85b7bf65dde65c79
Sabyasachi09 <sab.choubey@hotmail.com>
2024-06-10 21:02:24 +0530

    project setup

---
+++ src/test/java/com/myfirstbrew/Multithreading/MultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfirstbrew.Multithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
